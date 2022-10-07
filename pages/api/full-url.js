567897354d9277a3d702f510726834fb319b9023
Dledes <davidpewen_101@hotmail.com>
2022-10-06 23:42:37 -0300

    deleted js duplicated files

--- pages/api/full-url.js
+++
@@ -1,7 +0,0 @@
-
-
-//endpoint to get the full url, so any fetch made afterwards is properly pointed
-export default async function handler(req, res){
-    const fullUrl = req.headers.referer
-    res.status(200).json({success:true, data:fullUrl})
-}
